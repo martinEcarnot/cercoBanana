8fe25e238b3ebe27c535e0b99c673532654a01db
ngandinh12 <dinhhangngan@gmail.com>
2018-03-13 16:22:32 +0100

    signe_group 20180313

--- R/name.R
+++ R/name.R
@@ -1,12 +1,12 @@
 library(ggplot2)
 library(signal)
 
-source('D:\\Intern 2018\\Dinh\\scripts R\\SIGNE_load.R')  
-source('D:\\Intern 2018\\Dinh\\scripts R\\adj_asd.R')  
+source('D:\\Intern 2018\\cercoBanana\\R\\SIGNE_load.R')
+source('D:\\Intern 2018\\cercoBanana\\R\\adj_asd.R')
 
 id=read.table("D:\\Intern 2018\\Dinh\\Classeur fs5.csv",sep=",",header =TRUE)
 
-d="D:\\Intern 2018\\Dinh\\biov tous asd\\biov1 fs5 asd\\fs5 asd 2017 06 21"
+d="D:\\Intern 2018\\Dinh\\biov tous asd\\biov1 fs5 asd\\fs5 asd 2017 07 18"
 res0=SIGNE_load(d)
 
 # # Ajustement des sauts de detecteurs (Montpellier: sauts ?? 1000 (651??me l.o.) et 1800 (1451))
@@ -28,22 +28,25 @@ idtot=vector()
 
 for (i in 1:nrow(res))  {
   b=substr(nboit[i],1,nchar(nboit[i])-5)
-  ib=which(b==id$Pastille.boïte)
-  
-  id1=sprintf("%s_%9s_%s",substr(basename(d),9,18),id$Variété[ib],id$Boîte.ds.var[ib])
+  ib=which(b==id$Pastille.boite)
+
+  id1=sprintf("%s_%9s_%s",substr(basename(d),9,18),id$Variete[ib],id$Boite.ds.var[ib])
   idtot=rbind(idtot,id1)
 }
 
 iok = which(substr(idtot,17,19) == "T2." |substr(idtot,17,19) == "T3."|substr(idtot,17,19) == "T4."|substr(idtot,17,19) == "T5."|substr(idtot,17,19) == "T6."|substr(idtot,17,19) == "T7."|substr(idtot,17,19) == "T8."|substr(idtot,17,19) == "T9.")
 
 resok=res[iok,]
-geno=substr(idtot[iok],17,19)
+idtotok=idtot[iok]
+
+geno=substr(idtotok,17,19)
 rpca=prcomp(resok)
 
 #iout=[]
-#if (substr(basename(d),9,18)=="2017 06 21") {iout=which(rpca$x < -0.5)}
-#if (substr(basename(d),9,18)=="2017 07 06") {iout=which(rpca$x < -0.3)}
-iout=which(rpca$x < -0.5)
+#if (substr(basename(d),9,18)=="2017 06 21") {iout=which(rpca$x > 0.05)}
+#if (substr(basename(d),9,18)=="2017 07 06") {iout=which(rpca$x > 0.2)}
+if (substr(basename(d),9,18)=="2017 07 18") {iout=which(rpca$x < -0.02)}
+
 resok=resok[-iout,]
 geno=geno[-iout]
 rpca=prcomp(resok)
